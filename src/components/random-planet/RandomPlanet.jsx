8567889ed3ad6820033d6c0387eb0cced24086b2
Arthur <arturo.kudabaev@mail.ru>
2024-07-04 22:13:39 +0500

    api

--- src/components/random-planet/RandomPlanet.jsx
+++ src/components/random-planet/RandomPlanet.jsx
@@ -1,25 +1,52 @@
 import React from "react";
 import './random-planet.css';
+import SwapiService from "../../services/swapi-services";
 
 export default class RandomPlanet extends React.Component {
+
+    swapiService = new SwapiService();
+
+    state = {
+        planet: {},
+    }
+
+    constructor() {
+        super();
+        this.updatePlanet()
+    }
+
+    onPlanetLoaded = (planet) => {
+        this.setState({planet})
+    }
+
+    updatePlanet() {
+        const id = 12;
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+    }
+
     render() {
+
+        const {planet: {name,population,rotationPeriod,diameter}} = this.state;
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src="https://starwars-visualgiude.com/assets/img/planets/5.jpg" alt=""/>
+                     src={`https://starwars-visualguide.com/assets/img/planets/4.jpg`} alt=""/>
                 <div>
+                    <h4>{name}</h4>
                     <ul className='list-group list-group-flush'>
                         <li className='list-group-item'>
                             <span className='term'>Population</span>
-                            <span>123124</span>
+                            <span>{population}</span>
                         </li>
                         <li className='list-group-item'>
                             <span className='term'>Rotation Period</span>
-                            <span>43</span>
+                            <span>{rotationPeriod}</span>
                         </li>
                         <li className='list-group-item'>
                             <span className='term'>Diameter</span>
-                            <span>100</span>
+                            <span>{diameter}</span>
                         </li>
                     </ul>
                 </div>
