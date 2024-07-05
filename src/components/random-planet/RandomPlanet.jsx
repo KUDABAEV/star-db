6b119da6303de55adf495cf7d652593d3f941714
Arthur <arturo.kudabaev@mail.ru>
2024-07-05 19:43:15 +0500

    error-processing

--- src/components/random-planet/RandomPlanet.jsx
+++ src/components/random-planet/RandomPlanet.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './random-planet.css';
 import SwapiService from "../../services/swapi-services";
 import Spinner from "../spinner/Spinner";
+import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends React.Component {
 
@@ -10,6 +11,7 @@ export default class RandomPlanet extends React.Component {
     state = {
         planet: {},
         loading: true,
+        error: true
     }
 
     constructor() {
@@ -21,22 +23,37 @@ export default class RandomPlanet extends React.Component {
         this.setState({
             planet,
             loading:false,
+            error: false,
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading:false,
         })
     }
 
     updatePlanet() {
-        const id = 12;
+        const id = 5;
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {planet,loading} = this.state;
+        const {planet,loading,error} = this.state;
+
+        const hasData = !(loading || error);
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={planet} /> : null;
+        const content = hasData? <PlanetView planet={planet} /> : null;
+
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
@@ -49,7 +66,7 @@ const PlanetView = ({planet}) => {
     return (
         <>
             <img className="planet-image"
-                 src={`https://starwars-visualguide.com/assets/img/planets/4.jpg`} alt=""/>
+                 src={`https://starwars-visualguide.com/assets/img/planets/3.jpg`} alt=""/>
             <div>
                 <h4>{name}</h4>
                 <ul className='list-group list-group-flush'>
